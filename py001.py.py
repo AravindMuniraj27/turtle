a5201623507f09c3dddc2610acd84b7c79ed9146
Aravind <aravindmuniraj27@gmail.com>
2022-11-03 11:25:04 +0530

    'file1'

---
+++ py001.py.py
@@ -0,0 +1,18 @@
+print("student Record")
+x = int(input("student record based on Alphabet: "))
+if x>=1:
+    print("AA,AB,AC")
+    if x<10:
+        for a in ("Aravind","arun","aswin"):
+            print(a)
+        while x<10:
+            x=x+1
+            print("Aravind", str(x))
+for a in ["aravind", "arun","muniraj"]:
+    print(a)
+
+
+
+
+
+
---
+++ python-learning.py
@@ -0,0 +1,18 @@
+import turtle
+wm=turtle.Screen().bgcolor("white")
+t=turtle.Turtle()
+t.goto(0,200)
+a=0
+b=0
+t.speed(10)
+t.pencolor("black")
+while True:
+    t.forward(a)
+    t.right(b)
+    a=a+3
+    b=b+3
+    if b==210:
+        break
+        t.hidetrutle()
+
+        
