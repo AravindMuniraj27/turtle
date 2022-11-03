5a5352b10402a7aaa1a7aa504b31414ed7cfa184
Aravind <aravindmuniraj27@gmail.com>
2022-11-03 10:09:38 +0530

    sample commit

---
+++ sample.py
@@ -0,0 +1,12 @@
+txt = "aravind", "is a nice guy"
+if "guys" not in txt:
+    print("aravind")
+def myfunc():
+    a= "aravind"
+    print(len(a))
+    print(a[1])
+    print(a[1:6])
+    for x in "aravind":
+        print(x)
+myfunc()
+
